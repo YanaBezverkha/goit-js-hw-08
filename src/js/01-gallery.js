1b3f93161a60df22f3e21003e07a8c9441c581ac
YanaBezverkha <126686256+YanaBezverkha@users.noreply.github.com>
2023-05-31 13:22:04 +0100

    .

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const galleryMarkUp = galleryItems
+  .map(
+    img => `<li class="gallery__item">
+<a class="gallery__link" href="${img.original}"">
+  <img
+    class="gallery__image"
+    src="${img.preview}"
+    alt="${img.description}"
+  />
+</a>
+</li> `
+  )
+  .join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', galleryMarkUp);
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+gallery.on('show.simplelightbox', function onCloseModal(event) {
+  if (event.key === 'Escape') {
+    instance.close();
+  }
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle"
 
+const save = (key, value) => {
+    try {
+      const serializedState = JSON.stringify(value);
+      localStorage.setItem(key, serializedState);
+    } catch (error) {
+      console.error("Set state error: ", error.message);
+    }
+  };
+  
+  const load = key => {
+    try {
+      const serializedState = localStorage.getItem(key);
+      return serializedState === null ? undefined : JSON.parse(serializedState);
+    } catch (error) {
+      console.error("Get state error: ", error.message);
+    }
+  };
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+player.on('timeupdate', throttle(onPlay, 1000))
+
+function onPlay(evt){
+ save("videoplayer-current-time", evt.seconds);
+}
+const timePlayed = load("videoplayer-current-time")
+player.setCurrentTime(timePlayed)
